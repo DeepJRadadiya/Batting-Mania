85b0f81960c872feaacf993a9a4eaf2b67b2c02c
Deepjagdishbhairadadiya <deepradadiya0987@gmail.com>
2025-02-02 15:33:29 +0530

    audio file added

--- assets/js/Hilo.js
+++ assets/js/Hilo.js
@@ -10,18 +10,33 @@ let totalProfitScore = document.getElementById("totalProfitScore");
 let lowImg = document.getElementById("lowImg");
 let hiImg = document.getElementById("hiImg");
 let scoreingTab = document.getElementById("scoreingTab");
+let logUserId = localStorage.getItem("loggedinUserId");
+logUserId = logUserId.replace(/"/g, '').trim();
 let investVal = 0;
 let casoutState = false;
 let prevVal = null;
 let randVal = null;
 let score = 0;
-let logUserId = localStorage.getItem("loggedinUserId");
-logUserId = logUserId.replace(/"/g, '').trim();
+
+// all audio files
+const cardgenAudio = new Audio("../assets/audio/bet-4388e4b9.mp3");
+const casoutAudio = new Audio('../assets/audio/cashout-19360f23.mp3')
+const winAudio = new Audio("../assets/audio/win-88e8723b.mp3") 
+const lossAudio = new Audio('../assets/audio/deal-4c5ef928.mp3')
+const skipAudio = new Audio('../assets/audio/skip-639cbd65.mp3')
+cardgenAudio.preload = "auto";
+casoutAudio.preload = 'auto';
+winAudio.preload = 'auto';
+lossAudio.preload = 'auto';
+skipAudio.preload = 'auto';
 
 // disabel all btn
 const elements = [cardskipBtn1, cardskipBtn2, hiImg, lowImg];
 elements.forEach((el) => (el.style.cursor = "not-allowed"));
 
+
+
+// after batting button click
 function afterBattingClick() {
   elements.forEach((el) => (el.style.cursor = ""));
   Array.from(document.getElementsByClassName("blurDisplayEffect")).forEach(
@@ -40,6 +55,8 @@ function afterBattingClick() {
   betHandelerbtn.innerText = "Cash out";
 }
 
+
+// after casout button click
 function afterCasoutClick() {
   elements.forEach((el) => (el.style.cursor = ""));
   Array.from(document.getElementsByClassName("blurDisplayEffect")).forEach(
@@ -62,12 +79,16 @@ function afterCasoutClick() {
   casoutState = false;
   if (!(score === 0)) {
     alloverMoney.value = Number(alloverMoney.value) + score + investVal;
-    // moneyChanged(alloverMoney.value)
-    updateMoneyInDatabase(logUserId, Number(alloverMoney.value));
+      casoutAudio.play()
+    setTimeout(() => {
+      updateMoneyInDatabase(logUserId, Number(alloverMoney.value));
+    }, 5000);
   }
 
   totalProfitScore.innerHTML = 0;
+  setTimeout(() => {
   updateMoneyInDatabase(logUserId, Number(alloverMoney.value));
+  }, 4000);
 }
 
 //half button
@@ -102,19 +123,36 @@ doubletoMoney.addEventListener("click", () => {
   }
 });
 
+
+// randome nuber genrate
 function getRandomNumber() {
   return Math.floor(Math.random() * (13 - 1 + 1)) + 1;
 }
 
+//skip card gen
+function cardgenBtnHandlers() {
+  prevVal = randVal;
+  randVal = getRandomNumber();
+  skipAudio.play()
+  setTimeout(() => {
+    cardgenAudio.play()
+  }, 300);
+  HiloCardImg.src = "../assets/images/cards/" + randVal + ".png";
+}
+
+//all card gen
 function cardgenBtnHandler() {
   prevVal = randVal;
   randVal = getRandomNumber();
+  cardgenAudio.play()
   HiloCardImg.src = "../assets/images/cards/" + randVal + ".png";
 }
+
 // game handling function
 function gameHandler() {
   function scoreset() {
     if (score === 0) {
+      casoutState = false;
       hiImg.removeEventListener("click", hiImgHandler);
       lowImg.removeEventListener("click", lowImgHandler);
       cardskipBtn1.removeEventListener("click", cardgenBtnHandler);
@@ -132,37 +170,50 @@ function gameHandler() {
     }
   }
 
+  //hi button click
   function hiImgHandler() {
     cardgenBtnHandler();
     if (prevVal <= randVal) {
       console.log("high", prevVal, randVal);
       score += 1;
       scoreset();
+      cardgenAudio.pause()
+      winAudio.play()
+
     } else {
       console.log("game over");
       score = 0;
+      cardgenAudio.pause()
+      lossAudio.play()
       scoreset();
     }
   }
 
+  //low button click
   function lowImgHandler() {
     cardgenBtnHandler();
     if (prevVal >= randVal) {
         console.log("low", prevVal, randVal);
       score += 1;
+      cardgenAudio.pause()
+      winAudio.play()
       scoreset();
     } else {
         console.log("game over");
       score = 0;
+      cardgenAudio.pause()
+      lossAudio.play()
       scoreset();
     }
   }
-  cardskipBtn1.addEventListener("click", cardgenBtnHandler);
-  cardskipBtn2.addEventListener("click", cardgenBtnHandler);
+  cardskipBtn1.addEventListener("click", cardgenBtnHandlers);
+  cardskipBtn2.addEventListener("click", cardgenBtnHandlers);
   lowImg.addEventListener("click", lowImgHandler);
   hiImg.addEventListener("click", hiImgHandler);
 }
 
+
+//bet button click 
 betHandelerbtn.addEventListener("click", (event) => {
   event.preventDefault();
   let value = Number(batingMoney.value);
@@ -207,11 +258,8 @@ betHandelerbtn.addEventListener("click", (event) => {
     }, 500);
   }
 });
-// const moneyChanged = (moneyChangedValue) => {
-//   console.log("Changed money by:", moneyChangedValue);
-//   // updateMoneyInDatabase(logUserId, moneyChangedValue);
-// };
 
+// update money on databse when chnage the score
 async function updateMoneyInDatabase(id, moneyChangedValue) {
   try {
     const response = await fetch(`http://localhost:3000/users/${id}`, {
